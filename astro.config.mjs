d9dd23b31c6a74f806f1ba6d06255e8c448ba91c
Marat Yuldashev <marat.spb@protonmail.com>
2023-03-01 22:05:13 +0500

    Tailwind + restyling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://mir.github.io',
-    base: '/cv-site-astro',
+  site: 'https://mir.github.io',
+  base: '/cv-site-astro',
+  integrations: [tailwind()]
 });
