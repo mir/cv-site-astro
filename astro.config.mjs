bb3ac922c92090bc20596eda74b195e3cacb0ae6
Marat Yuldashev <marat.spb@protonmail.com>
2023-03-02 13:44:28 +0500

    2 cvs now

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mir.github.io',
-  base: '/cv-site-astro',
+  base: '/',
   integrations: [tailwind()]
 });
